819d71cfbaac772e41c3518773a919ea52a975a5
semih babacan huawei <m.semihbabacan@gmail.com>
2023-11-26 03:01:43 +0300

    Update: udpated the config of build.gradle file based on the requiremets of google ml toolkit text recognition package

---
+++ lib/presentation/screen/main/sub_screens/camera_control_additives.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CameraControlAdditivesScreen extends StatefulWidget {
+  const CameraControlAdditivesScreen({super.key});
+
+  @override
+  State<CameraControlAdditivesScreen> createState() => _CameraControlAdditivesScreenState();
+}
+
+class _CameraControlAdditivesScreenState extends State<CameraControlAdditivesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
